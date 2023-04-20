8dea8830a39e71acc97129e14accfdb161316d00
edmondgaks <edmondgaks@gmail.com>
2023-04-20 16:43:43 +0200

    adding search

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -1,13 +1,16 @@
+'use client';
 import Container from "../Container"
 import Logo from "./Logo"
+import Search from "./Search"
 
 const Navbar = () => {
     return (
         <div className="fixed w-full shadow-sm bg-white z-10">
             <div className="py-4 border-b-[1px]">
                 <Container>
-                    <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+                    <div className="flex flex-row items-center justify-between gap-3 md:gap-3">
                         <Logo />
+                        <Search />
                     </div>
                 </Container>
             </div>
