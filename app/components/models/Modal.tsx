dae2541d65977d22f1868cb26856a7db346719c8
edmondgaks <edmondgaks@gmail.com>
2023-04-21 12:50:43 +0200

    adding inputs and corecting body

--- app/components/models/Modal.tsx
+++ app/components/models/Modal.tsx
@@ -8,7 +8,7 @@ interface ModalProps {
     onClose: () => void;
     onSubmit: () => void;
     title?: string;
-    body?: string;
+    body?: React.ReactElement;
     footer?: React.ReactElement;
     actionLabel: string;
     disabled?: boolean;
