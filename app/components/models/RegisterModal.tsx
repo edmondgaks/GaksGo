b253121a8abe1762887d5d0ce08948356ee631da
edmondgaks <edmondgaks@gmail.com>
2023-04-21 15:21:04 +0200

    footer

--- app/components/models/RegisterModal.tsx
+++ app/components/models/RegisterModal.tsx
@@ -9,6 +9,7 @@ import { toast } from "react-hot-toast";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import Button from "../Button";
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
     const [isLoading, setIsLoading] = useState(false);
@@ -33,12 +34,33 @@ const RegisterModal = () => {
             .then(() => {
                 registerModal.onClose();
             }).catch((error) => {
-                console.log(error)
+                toast.error("Something went wrong")
             }).finally(() => {
                 setIsLoading(false)
             })
     }
-
+    const FooterContent = (
+        <div className="flex flex-col gap-4 mt-3">
+            <hr />
+            <Button 
+                outline 
+                label="Continue with Google"
+                icon={FcGoogle}
+                onClick={() =>{}} 
+            />
+            <Button 
+                outline 
+                label="Continue with Github"
+                icon={AiFillGithub}
+                onClick={() => {}}
+            />
+            <div className="text-neutral-500 text-center mt-4 font-light">
+                <p>Already have an account?
+                    <span onClick={() => {}} className="text-neutral-800 cursor-pointer hover:underline"> Log in</span>
+                </p>
+        </div>
+      </div>
+    )
     const bodyContent = (
         <div className="flex flex-col gap-4">
             <Heading title="Welcome to Airbnb" subtitle="Create an account" center />
@@ -79,6 +101,7 @@ const RegisterModal = () => {
             onClose={registerModal.onClose}
             onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
+            footer={FooterContent}
         />
     )
 }
