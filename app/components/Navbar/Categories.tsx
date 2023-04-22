973cbc54e015fa879260141f5c5208622a439fee
edmondgaks <edmondgaks@gmail.com>
2023-04-22 19:55:51 +0200

    adding the categories well

--- app/components/Navbar/Categories.tsx
+++ app/components/Navbar/Categories.tsx
@@ -15,6 +15,9 @@ import { FaSkiing } from 'react-icons/fa';
 import { BsSnow } from 'react-icons/bs';
 import { IoDiamond } from 'react-icons/io5';
 import { MdOutlineVilla } from 'react-icons/md';
+
+import CategoryBox from "../CategoryBox";
+
 export const categories = [
     {
       label: 'Beach',
@@ -94,9 +97,24 @@ export const categories = [
   ]
   
 const Categories = () => {
+    const params = useSearchParams();
+    const category = params?.get('category');
+    const pathname = usePathname();
+    const isMainPage = pathname === '/';
     return (
         <Container>
-
+            <div
+                className="pt-4 flex flex-row items-center justify-between overflow-x-auto"
+      >
+        {categories.map((item) => (
+          <CategoryBox 
+            key={item.label}
+            label={item.label}
+            icon={item.icon}
+            selected={category === item.label}
+          />
+        ))}
+      </div>
         </Container>
     )
 }
