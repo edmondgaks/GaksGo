bb089caf971604c619ae6ecb66c121bbc06c08d2
edmondgaks <edmondgaks@gmail.com>
2023-04-26 20:46:24 +0200

    adding the heart button

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -4,8 +4,12 @@ import Categories from "./Categories";
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu";
+import SafeUser from "../../../app/types";
+interface NavbarProps {
+    currentUser?: SafeUser | null;
+  }
 
-const Navbar = () => {
+const Navbar = ({ currentUser }) => {
     return (
         <div className="fixed w-full shadow-sm bg-white z-10">
             <div className="py-4 border-b-[1px]">
@@ -13,7 +17,7 @@ const Navbar = () => {
                     <div className="flex flex-row items-center justify-between gap-3 md:gap-3">
                         <Logo />
                         <Search />
-                        <UserMenu />
+                        <UserMenu currentUser={currentUser} />
                     </div>
                 </Container>
             </div>
